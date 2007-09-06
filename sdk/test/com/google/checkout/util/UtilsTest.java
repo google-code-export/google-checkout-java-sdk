e3bbaceb4f2e27224bbce825b45fdddf21af9124
dang.charles <dang.charles@29c223f6-fc33-0410-bd55-517fb383b6be>
2007-09-06 17:34:40 +0000

    fixed the build scripts to copy the indent.xsl into the build classes directory

---
+++ sdk/test/com/google/checkout/util/UtilsTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (C) 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package com.google.checkout.util;
+
+import java.io.File;
+import junit.framework.TestCase;
+
+import java.io.InputStream;
+
+import javax.xml.transform.TransformerException;
+
+import java.io.IOException;
+
+/**
+ * Tests for the Utils.java class
+ *
+ * @author cdang
+ */
+public class UtilsTest extends TestCase {
+  
+  /**
+   * small test to verify indent.xsl exists under the src directory
+   */
+  public void testIndentXslExists() {
+    InputStream inputStream = Utils.class.getResourceAsStream("indent.xsl");
+    
+    assertTrue(null != inputStream);
+  }
+}
