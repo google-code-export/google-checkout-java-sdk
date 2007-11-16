b164dd48f3e8a73d0677c8f0359874e17e616c23
dang.charles <dang.charles@29c223f6-fc33-0410-bd55-517fb383b6be>
2007-11-16 22:00:37 +0000

    Fixing the Google copyright header

--- trunk/sdk/src/java/com/google/checkout/handlers/CompositeNotificationHandler.java
+++ trunk/sdk/src/java/com/google/checkout/handlers/CompositeNotificationHandler.java
@@ -1,20 +1,18 @@
-  /**
-   * Parses an xml string and returns a CheckoutNotification object of the given
-   * type.
-   * 
-   * Assumes the root element contains the notification type. If the notification
-   * type is not one of the following types, an UnknownNotificationException is thrown:
-   * new-order-notification, risk-information-notification, 
-   * order-state-change-notification, refund-amount-notification, 
-   * chargeback-amount-notification or authorization-amount-notification.
-   * 
-   * @param xmlString
-   * @return A notification object of the specified type
-   * @throws com.google.checkout.notification.UnknownNotificationException if the
-   * notification type was not recognized
-   * @throws com.google.checkout.notification.exceptions.CheckoutException if 
-   * there was an error parsing the request string
-   */
+/*******************************************************************************
+ * Copyright (C) 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
 
 package com.google.checkout.handlers;
 
