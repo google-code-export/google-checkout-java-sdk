3b689202ac3a06d466bbdf1b1ef330441c96527a
wt@penguintechs.org <wt@penguintechs.org@29c223f6-fc33-0410-bd55-517fb383b6be>
2009-09-26 10:40:04 +0000

    Enable Maven builds on the sdk.
    I moved all the source into the default locations for the Maven based
    build tools.

---
+++ sdk/src/main/java/com/google/checkout/util/Utils.java
@@ -0,0 +1,463 @@
+/*******************************************************************************
+ * Copyright (C) 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package com.google.checkout.util;
+
+import com.google.checkout.CheckoutException;
+import com.google.checkout.CheckoutSystemException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.TimeZone;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Various XML utilities.
+ * 
+ * @author simonjsmith, ksim
+ * @version 1.1 - ksim - March 6th, 2007 - Added functions regarding streaming
+ * @version 1.2 - ksim - March 10th, 2007 - Added functions regarding DOM
+ *          manipulation
+ */
+
+public class Utils {
+
+  public static Document newEmptyDocument() {
+    DocumentBuilderFactory factory = null;
+    DocumentBuilder builder = null;
+    Document ret;
+
+    try {
+      factory = DocumentBuilderFactory.newInstance();
+      builder = factory.newDocumentBuilder();
+    } catch (ParserConfigurationException ex) {
+      throw new CheckoutSystemException("Could not create new empty document.");
+    }
+
+    ret = builder.newDocument();
+
+    return ret;
+  }
+
+  public static Element findElementOrContainer(Document document,
+      Element parent, String element) {
+    NodeList nl = parent.getElementsByTagName(element);
+    if (nl.getLength() == 0) {
+      return null;
+    }
+    return (Element) nl.item(0);
+  }
+
+  public static Element findContainerElseCreate(Document document,
+      Element parent, String child) {
+    NodeList nl = parent.getElementsByTagName(child);
+    if (nl.getLength() == 0) {
+      parent.appendChild(document.createElement(child));
+    }
+    return (Element) parent.getElementsByTagName(child).item(0);
+  }
+
+  public static Element createNewContainer(Document document, Element parent,
+      String childElement) {
+    Element child = document.createElement(childElement);
+    parent.appendChild(child);
+    return child;
+  }
+
+  public static Element findElementElseCreateAndSet(Document document,
+      Element parent, String child, String value) {
+    Element ret = null;
+    NodeList nl = parent.getElementsByTagName(child);
+    if (nl.getLength() == 0) {
+      parent.appendChild(document.createElement(child));
+      ret = (Element) parent.getElementsByTagName(child).item(0);
+      ret.appendChild(document.createTextNode(value));
+    }
+    return ret;
+  }
+
+  public static Element findElementElseCreateAndSet(Document document,
+      Element parent, String child, boolean value) {
+    return findElementElseCreateAndSet(document, parent, child, value + "");
+  }
+
+  public static Element findElementAndSetElseCreateAndSet(Document document,
+      Element parent, String child, String value) {
+    NodeList nl = parent.getElementsByTagName(child);
+    if (nl.getLength() == 0) {
+      parent.appendChild(document.createElement(child));
+    }
+    Element ret = (Element) parent.getElementsByTagName(child).item(0);
+    if (ret.getFirstChild() != null) {
+      ret.removeChild(ret.getFirstChild());
+    }
+    ret.appendChild(document.createTextNode(value));
+    return ret;
+  }
+
+  public static Element findElementAndSetElseCreateAndSet(Document document,
+      Element parent, String child, boolean value) {
+    return findElementAndSetElseCreateAndSet(document, parent, child, ""
+        + value);
+  }
+
+  public static Element findElementAndSetElseCreateAndSet(Document document,
+      Element parent, String child, float value) {
+    return findElementAndSetElseCreateAndSet(document, parent, child,
+        ""+value);
+  }
+
+  public static Element findElementAndSetElseCreateAndSet(Document document,
+      Element parent, String child, int value) {
+    return findElementAndSetElseCreateAndSet(document, parent, child, ""+value);
+  }
+
+  public static Element createNewElementAndSet(Document document,
+      Element parent, String childElement, String childValue) {
+    Element child = document.createElement(childElement);
+    parent.appendChild(child);
+    child.setNodeValue(childValue);
+    child.appendChild(document.createTextNode(childValue));
+    return child;
+  }
+
+  public static Element createNewElementAndSetAndAttribute(Document document,
+      Element parent, String childElement, String childValue,
+      String attributeName, String attributeValue) {
+    Element child =
+        createNewElementAndSet(document, parent, childElement, childValue);
+    child.setAttribute(attributeName, attributeValue);
+    return child;
+  }
+
+  public static Element createNewElementAndSet(Document document,
+      Element parent, String childElement, float childValue) {
+    return createNewElementAndSet(document, parent, childElement, ""+childValue);
+  }
+
+  public static Element createNewElementAndSet(Document document,
+      Element parent, String childElement, int childValue) {
+    return createNewElementAndSet(document, parent, childElement, ""+childValue);
+  }
+
+  public static Element createNewElementAndSet(Document document,
+      Element parent, String childElement, boolean childValue) {
+    return createNewElementAndSet(document, parent, childElement, ""
+        + childValue);
+  }
+
+  public static Element createNewElementAndSet(Document document, Element parent,
+      String childElement, double childValue) {
+    return createNewElementAndSet(document, parent, childElement, ""+childValue);
+  }
+
+  public static String getElementStringValue(Document document, Element parent,
+      String element) {
+    NodeList nl = parent.getElementsByTagName(element);
+    if (nl.getLength() == 0) {
+      return "";
+    }
+
+    Node n = nl.item(0).getFirstChild();
+    if (n == null) {
+      return "";
+    }
+
+    return n.getNodeValue().trim();
+  }
+
+  public static boolean getElementBooleanValue(Document document,
+      Element parent, String element) {
+    return Boolean.valueOf(getElementStringValue(document, parent, element))
+        .booleanValue();
+  }
+
+  public static float getElementFloatValue(Document document, Element parent,
+      String element) {
+    return Float.parseFloat(getElementStringValue(document, parent, element));
+  }
+
+  public static void importElements(Document document, Element parent,
+      Element[] children) {
+    for (int i = 0; i < children.length; i++) {
+      parent.appendChild(document.importNode(children[i], true));
+    }
+  }
+
+  public static Date getElementDateValue(Document document, Element parent,
+      String string) throws CheckoutException {
+    return parseDate(getElementStringValue(document, parent, string));
+  }
+
+  public static long getElementLongValue(Document document, Element parent,
+      String string) {
+    return Long.parseLong(getElementStringValue(document, parent, string));
+  }
+
+  public static int getElementIntValue(Document document, Element parent,
+      String string) {
+    return Integer.parseInt(getElementStringValue(document, parent, string));
+  }
+
+  public static Element findElementAndSetElseCreateAndSet(Document document,
+      Element parent, String element, Date date) {
+    return findElementAndSetElseCreateAndSet(document, parent, element,
+        getDateString(date));
+  }
+
+  public static Element[] getElements(Document document, Element parent) {
+    if (parent == null) {
+      return new Element[] {};
+    }
+
+    NodeList nl = parent.getChildNodes();
+    ArrayList al = new ArrayList();
+
+    for (int i = 0; i < nl.getLength(); i++) {
+      Node n = nl.item(i);
+
+      if (n instanceof Element) {
+        al.add(nl.item(i));
+      }
+    }
+
+    Element[] ret = new Element[al.size()];
+    Iterator it = al.iterator();
+    int i = 0;
+    while (it.hasNext()) {
+      ret[i] = (Element) it.next();
+      i++;
+    }
+
+    return ret;
+  }
+
+  public static Element findContainerWithAttributeValueElseCreate(
+      Document document, Element parent, String element, String attributeName,
+      String attributeValue) {
+
+    NodeList nl = parent.getElementsByTagName(element);
+    Element e;
+    for (int i = 0; i < nl.getLength(); i++) {
+      e = (Element) nl.item(i);
+      if (e.getAttribute(attributeName).equals(attributeValue)) {
+        return e;
+      }
+    }
+
+    e = document.createElement(element);
+    parent.appendChild(e);
+    e.setAttribute(attributeName, attributeValue);
+
+    return e;
+  }
+
+  public static Element findContainerWithAttributeValueElseCreateAndSet(
+      Document document, Element parent, String element, String value,
+      String attributeName, String attributeValue) {
+
+    Element e =
+        findContainerWithAttributeValueElseCreate(document, parent, element,
+            attributeName, attributeValue);
+    e.appendChild(document.createTextNode(value));
+
+    return e;
+  }
+
+  public static Element findElementElseCreateAndAttribute(Document document,
+      Element parent, String element, String attributeName,
+      String attributeValue) {
+    NodeList nl = parent.getElementsByTagName(element);
+    Element e = null;
+
+    if (nl.getLength() == 0) {
+      parent.appendChild(document.createElement(element));
+      e = (Element) parent.getElementsByTagName(element).item(0);
+      e.setAttribute(attributeName, attributeValue);
+    }
+
+    return e;
+  }
+
+  public static Element findElementElseCreateAndSetAndAttribute(
+      Document document, Element parent, String element, String value,
+      String attributeName, String attributeValue) {
+
+    Element e =
+        findElementElseCreateAndAttribute(document, parent, element,
+            attributeName, attributeValue);
+    if (e != null) e.appendChild(document.createTextNode(value));
+
+    return e;
+  }
+  
+  /**
+   * Return a string representation of an org.w3c.dom.Node.
+   * 
+   * @param node The XML node.
+   * @return The string representation of the XML doc.
+   */
+  public static String nodeToString(Node node) {
+    try {
+      TransformerFactory tf = TransformerFactory.newInstance();
+      Transformer trans = tf.newTransformer();
+      StringWriter sw = new StringWriter();
+      trans.transform(new DOMSource(node), new StreamResult(sw));
+      return sw.toString();
+    } catch (TransformerException ex) {
+      throw new CheckoutSystemException("Unable to convert document to string");
+    }
+  }
+  
+  /**
+   * Return a string representation of an org.w3c.dom.Document.
+   * 
+   * @param document The XML document.
+   * @deprecated Use {@link #nodeToString(Node)} instead.
+   * @return The string representation of the XML doc.
+   */
+  @Deprecated
+  public static String documentToString(Document document) {
+    return nodeToString(document);
+  }
+
+  public static String documentToStringPretty(Document document) {    
+    try {
+      StreamSource stylesource =
+        new StreamSource(Utils.class.getResourceAsStream("/com/google/checkout/util/indent.xsl"));
+
+      TransformerFactory tf = TransformerFactory.newInstance();
+      Transformer trans = tf.newTransformer(stylesource);
+
+      StringWriter sw = new StringWriter();
+      trans.transform(new DOMSource(document), new StreamResult(sw));
+
+      return sw.toString();
+    } catch (TransformerException ex) {
+      throw new CheckoutSystemException("Unable to convert document to string");
+    }
+  }
+
+  public static Document newDocumentFromString(String xmlString) 
+    throws CheckoutException {
+    DocumentBuilderFactory factory = null;
+    DocumentBuilder builder = null;
+    Document ret = null;
+
+    try {
+      factory = DocumentBuilderFactory.newInstance();
+      builder = factory.newDocumentBuilder();
+    } catch (ParserConfigurationException ex) {
+      throw new CheckoutSystemException(ex.getMessage());
+    }
+
+    try {
+      ret = builder.parse(new InputSource(new StringReader(xmlString)));
+    } catch (SAXException ex) {
+      throw new CheckoutException("Got the following error when attempting to " +
+        "create new document from the given xml string. Error message: " 
+        + ex.getMessage());
+    } catch (IOException ex) {
+      throw new CheckoutSystemException("Error creating new document from the " +
+        "specified string. Error message: " + ex.getMessage());
+    }
+    
+    return ret;
+  }
+
+  public static Document newDocumentFromInputStream(InputStream in) 
+    throws CheckoutException {
+    DocumentBuilderFactory factory = null;
+    DocumentBuilder builder = null;
+    Document ret = null;
+
+    try {
+      factory = DocumentBuilderFactory.newInstance();
+      builder = factory.newDocumentBuilder();
+    } catch (ParserConfigurationException ex) {
+      throw new CheckoutSystemException(ex.getMessage());
+    }
+
+    try {
+      ret = builder.parse(new InputSource(in));
+    } catch (SAXException ex) {
+      throw new CheckoutException("Got the following error when attempting to " + 
+        "create new document from input stream. Error message: " 
+        + ex.getMessage());
+    } catch (IOException ex) {
+      throw new CheckoutSystemException("Error creating new document from the " +
+        "specified input stream. Error message: " + ex.getMessage());
+    }
+    
+    return ret;
+  }
+
+  private static SimpleDateFormat sdf =
+      new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+  
+  //Ugh, ISO8601 Date formatting is quite ugly...
+  public static Date parseDate(String date) throws CheckoutException {
+    try {
+      if (date.endsWith("Z")) {
+        date = date.substring(0, date.length() - 1) + "+00:00";       
+      }
+      //remove the ":" in the timezone offset.
+      if (":".equals(date.substring(date.length()-3, date.length()-2))) {
+        date = date.substring(0, date.length()-3) + date.substring(date.length()-2, date.length());
+      }
+            
+      return sdf.parse(date);
+    } catch (ParseException ex) {
+      throw new CheckoutException("Could not parse date: " + date + 
+        ". The expected format is " + sdf.toPattern());
+    }
+  }
+
+  public static String getDateString(Date date) {
+    if (date == null) {
+      return "null";
+    }
+    
+    TimeZone tz = TimeZone.getTimeZone( "UTC" );
+    sdf.setTimeZone( tz );
+    String ret = sdf.format(date);
+    
+    return ret.substring(0, ret.length()-5)+"Z";
+  }
+}
---
+++ sdk/src/test/java/com/google/checkout/notification/NewOrderNotificationTest.java
@@ -0,0 +1,203 @@
+/*******************************************************************************
+ * Copyright (C) 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package com.google.checkout.notification;
+
+import com.google.checkout.CheckoutException;
+import com.google.checkout.checkout.Item;
+import com.google.checkout.util.TestUtils;
+import com.google.checkout.util.Utils;
+
+import junit.framework.TestCase;
+
+import org.w3c.dom.Document;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+
+public class NewOrderNotificationTest extends TestCase {
+  private String newOrderNotificationMessage;
+
+  private NewOrderNotification newOrderNotification;
+
+  @Override
+  public void setUp() {
+    newOrderNotificationMessage =
+        TestUtils.readMessage("/new-order-notification-sample.xml");
+
+    try {
+      Document doc = Utils.newDocumentFromString(newOrderNotificationMessage);
+
+      newOrderNotification = new NewOrderNotification(doc);
+    } catch (CheckoutException ex) {
+      fail();
+    }
+  }
+
+  public void testGetShoppingCart() {
+    ShoppingCart cart = newOrderNotification.getShoppingCart();
+    try {
+      Date cartExpDate = cart.getCartExpiration();
+      assertEquals("2007-12-31T23:59:59.000Z", Utils.getDateString(cartExpDate));
+
+      verfifyShoppingCartItems(cart.getItems());
+    } catch (CheckoutException ex) {
+      fail();
+    }
+  }
+
+  public void testGetBuyerShippingAddress() {
+    Address shippingAddress = newOrderNotification.getBuyerShippingAddress();
+
+    assertEquals("John Smith", shippingAddress.getContactName());
+    assertEquals("johnsmith@example.com", shippingAddress.getEmail());
+    assertEquals("10 Example Road", shippingAddress.getAddress1());
+    assertEquals("Sampleville", shippingAddress.getCity());
+    assertEquals("CA", shippingAddress.getRegion());
+    assertEquals("94141", shippingAddress.getPostalCode());
+    assertEquals("US", shippingAddress.getCountryCode());
+  }
+
+  public void testGetBuyerBillingAddress() {
+    Address billingAddress = newOrderNotification.getBuyerBillingAddress();
+
+    assertEquals("Bill Hu", billingAddress.getContactName());
+    assertEquals("billhu@example.com", billingAddress.getEmail());
+    assertEquals("99 Credit Lane", billingAddress.getAddress1());
+    assertEquals("Mountain View", billingAddress.getCity());
+    assertEquals("CA", billingAddress.getRegion());
+    assertEquals("94043", billingAddress.getPostalCode());
+    assertEquals("US", billingAddress.getCountryCode());
+  }
+
+  public void testGetBuyerMarketingPreferences() {
+    BuyerMarketingPreferences preferences =
+        newOrderNotification.getBuyerMarketingPreferences();
+    assertFalse(preferences.isMarketingEmailAllowed());
+  }
+
+  public void testGetOrderAdjustment() {
+    OrderAdjustment adjustment = newOrderNotification.getOrderAdjustment();
+    assertTrue(adjustment.isMerchantCalculationSuccessful());
+
+    verifyMerchantCodes(adjustment.getMerchantCodes());
+
+    Shipping shipping = adjustment.getShipping();
+    assertEquals(9.95f, shipping.getShippingCost(), 0);
+    assertEquals("SuperShip", shipping.getShippingName());
+
+    assertEquals(11.05f, adjustment.getTotalTax(), 0);
+  }
+
+  public void testGetOrderTotal() {
+    assertEquals(190.98f, newOrderNotification.getOrderTotal(), 0);
+  }
+
+  public void testGetOrderCurrencyCode() {
+    assertEquals("USD", newOrderNotification.getOrderCurrencyCode());
+  }
+
+  public void testGetFulfillmentOrderState() {
+    FulfillmentOrderState state =
+        newOrderNotification.getFulfillmentOrderState();
+    assertEquals("NEW", state.toString());
+  }
+
+  public void testGetFinancialOrderState() {
+    FinancialOrderState state = newOrderNotification.getFinancialOrderState();
+    assertEquals("REVIEWING", state.toString());
+  }
+
+  public void testGetBuyerId() {
+    assertEquals(294873009217523L, newOrderNotification.getBuyerId());
+  }
+
+  private void verfifyShoppingCartItems(Collection items) {
+    Iterator it = items.iterator();
+
+    Item item1 = (Item) it.next();
+    assertEquals("GGLAA1453", item1.getMerchantItemId());
+    assertEquals("Dry Food Pack", item1.getItemName());
+    assertEquals("One pack of nutritious dried food for emergencies.", item1
+        .getItemDescription());
+    assertEquals(1, item1.getQuantity());
+    assertEquals("food", item1.getTaxTableSelector());
+    assertEquals(4.99f, item1.getUnitPriceAmount(), 0);
+    assertEquals("USD", item1.getUnitPriceCurrency());
+
+    Item item2 = (Item) it.next();
+    assertEquals("MGS2GBMP3", item2.getMerchantItemId());
+    assertEquals("Megasound 2GB MP3 Player", item2.getItemName());
+    assertEquals("This portable MP3 player stores 500 songs.", item2
+        .getItemDescription());
+    assertEquals(1, item2.getQuantity());
+    assertEquals(179.99f, item2.getUnitPriceAmount(), 0);
+    assertEquals("USD", item2.getUnitPriceCurrency());
+  }
+
+  private void verifyMerchantCodes(Collection merchantCodes) {
+    Iterator it = merchantCodes.iterator();
+
+    CouponAdjustment couponAdjustment = (CouponAdjustment) it.next();
+    assertEquals(5.00f, couponAdjustment.getAppliedAmount(), 0);
+    assertEquals("FirstVisitCoupon", couponAdjustment.getCode());
+    assertEquals(5.00f, couponAdjustment.getCalculatedAmount(), 0);
+    assertEquals("You saved $5.00 for your first visit!", couponAdjustment
+        .getMessage());
+
+    GiftCertificateAdjustment giftAdjustment =
+        (GiftCertificateAdjustment) it.next();
+    assertEquals(10.00f, giftAdjustment.getAppliedAmount(), 0);
+    assertEquals("GiftCert12345", giftAdjustment.getCode());
+    assertEquals(10.00f, giftAdjustment.getCalculatedAmount(), 0);
+    assertEquals("You saved $10.00 with this gift certificate!", giftAdjustment
+        .getMessage());
+
+  }
+
+  public void testIsSerializable() throws IOException {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(out);
+    oos.writeObject(newOrderNotification);
+    oos.close();
+    assertTrue(out.toByteArray().length > 0);
+  }
+  
+  public void testStructuredName() {
+    String newOrderNotificationMessage2 = TestUtils.readMessage("/new-order-notification-sample-with-structured-name.xml");
+
+  try {
+    Document doc = Utils.newDocumentFromString(newOrderNotificationMessage2);
+
+    NewOrderNotification newOrderNotification2 = new NewOrderNotification(doc);
+    StructuredName ship = newOrderNotification2.getBuyerShippingAddress().getStructuredName();
+    StructuredName bill = newOrderNotification2.getBuyerBillingAddress().getStructuredName();
+    
+    assertEquals(ship.getFirstName(), "John");
+    assertEquals(ship.getLastName(), "Smith");
+    assertEquals(bill.getFirstName(), "John");
+    assertEquals(ship.getLastName(), "Smith"); 
+    
+  } catch (CheckoutException ex) {
+    fail();
+  }
+  }
+}
