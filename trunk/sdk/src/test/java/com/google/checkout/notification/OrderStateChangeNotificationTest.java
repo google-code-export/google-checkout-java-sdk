6f255f6e2299568ad315111334fe24e784b75983
wt@penguintechs.org <wt@penguintechs.org@29c223f6-fc33-0410-bd55-517fb383b6be>
2009-09-26 10:40:04 +0000

    Enable Maven builds on the sdk.
    I moved all the source into the default locations for the Maven based
    build tools.

---
+++ trunk/sdk/src/test/java/com/google/checkout/notification/OrderStateChangeNotificationTest.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (C) 2007 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+
+package com.google.checkout.notification;
+
+import com.google.checkout.CheckoutException;
+import com.google.checkout.util.TestUtils;
+import com.google.checkout.util.Utils;
+
+import junit.framework.TestCase;
+
+import org.w3c.dom.Document;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class OrderStateChangeNotificationTest extends TestCase {
+  private String orderStateChangeNotificationMessage;
+
+  private OrderStateChangeNotification orderStateChangeNotification;
+
+  @Override
+  public void setUp() {
+    orderStateChangeNotificationMessage =
+        TestUtils
+            .readMessage("/order-state-change-notification-sample.xml");
+
+    try {
+      Document doc =
+          Utils.newDocumentFromString(orderStateChangeNotificationMessage);
+
+      orderStateChangeNotification = new OrderStateChangeNotification(doc);
+    } catch (CheckoutException ex) {
+      fail();
+    }
+  }
+
+  public void testGetNewFulfillmentOrderState() {
+    FulfillmentOrderState state =
+        orderStateChangeNotification.getNewFulfillmentOrderState();
+
+    assertEquals("NEW", state.toString());
+  }
+
+  public void testGetNewFinancialOrderState() {
+    FinancialOrderState state =
+        orderStateChangeNotification.getNewFinancialOrderState();
+
+    assertEquals("CHARGING", state.toString());
+  }
+
+  public void testGetPreviousFulfillmentOrderState() {
+    FulfillmentOrderState state =
+        orderStateChangeNotification.getPreviousFulfillmentOrderState();
+
+    assertEquals("NEW", state.toString());
+  }
+
+  public void testGetPreviousFinancialOrderState() {
+    FinancialOrderState state =
+        orderStateChangeNotification.getPreviousFinancialOrderState();
+
+    assertEquals("CHARGEABLE", state.toString());
+  }
+
+  public void testIsSerializable() throws IOException {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ObjectOutputStream oos = new ObjectOutputStream(out);
+    oos.writeObject(orderStateChangeNotification);
+    oos.close();
+    assertTrue(out.toByteArray().length > 0);
+  }
+}
